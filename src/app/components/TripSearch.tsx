ca3b565ee68aaa1e3f91bcc7a104358a285753d0
Bruno Seifert <128076381+brunoseifert@users.noreply.github.com>
2023-07-11 13:59:40 -0300

    Button

--- src/app/components/TripSearch.tsx
+++ src/app/components/TripSearch.tsx
@@ -1,4 +1,6 @@
 "use client";
+
+import Button from '@/components/Button';
 import CurrencyInput from '@/components/CurrencyInput';
 import DatePicker from '@/components/DatePicker';
 import Input from '@/components/Input';
@@ -7,7 +9,7 @@ import React from 'react';
 
 const TripSearch = () =>{
     return(
-        <div className='container mx-auto p-5'>
+        <div className="container mx-auto p-5 bg-search-background bg-cover bg-center bg-no-repeat lg:py-28">
             <h1 className='font-semibold text-2xl text-primaryDarker text-center'>
                 Encontre sua próxima <span className='text-primary'>viagem</span>
             </h1>
@@ -20,6 +22,10 @@ const TripSearch = () =>{
             <DatePicker placeholderText='Data de ida' onChange={() => {}} className='w-full'/>
             <CurrencyInput placeholder='Orçamento'/>
         </div>
+
+        <Button  className="w-full my-5 lg:h-fit">
+          Buscar
+        </Button>
     </div>
     );
 
