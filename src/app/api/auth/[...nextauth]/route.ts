c8d76c5cddf3b46082afda5d860ae02caa0f29ce
JonathanAmarall <jhouamaral95@gmail.com>
2023-07-12 15:03:22 -0300

    fix route and seed

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,20 +1,6 @@
-import NextAuth, {AuthOptions} from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { Adapter } from "next-auth/adapters";
+import NextAuth from 'next-auth';
+import { authOptions } from '../../../../../utils/authOptions ';
 
-import {prisma} from "@/lib/prisma";
+const handler = NextAuth(authOptions);
 
-export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(prisma) as Adapter,
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-};
-
-const handler= NextAuth(authOptions);
-
-export {handler as GET, handler as POST};
+export { handler as GET, handler as POST };
