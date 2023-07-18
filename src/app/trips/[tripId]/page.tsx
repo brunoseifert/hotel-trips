a8c5265edf9af5822046ed4787a388dee7d3cec1
Bruno Seifert <128076381+brunoseifert@users.noreply.github.com>
2023-07-18 11:04:38 -0300

    description trips

--- src/app/trips/[tripId]/page.tsx
+++ src/app/trips/[tripId]/page.tsx
@@ -1,25 +1,45 @@
+import { prisma } from "@/lib/prisma";
+
 import React from "react";
-import {prisma} from '@/lib/prisma';
-import TripHeader from "../components/TripHeader";
-
-
+import TripHeader from "./components/TripHeader";
+import TripReservation from "./components/TripReservation";
+import TripDescription from "./components/TripDescription";
 
 const getTripDetails = async (tripId: string) => {
-    const trip = await prisma.trip.findUnique({
-        where: {
-            id:tripId
-        },
-    });
+  const trip = await prisma.trip.findUnique({
+    where: {
+      id: tripId,
+    },
+  });
 
-    return trip;
+  return trip;
 };
 
-const TripDetails = async ({params} : {params: {tripId: string}}) => {
-    const trip = await getTripDetails(params.tripId)
+const TripDetails = async ({ params }: { params: { tripId: string } }) => {
+  const trip = await getTripDetails(params.tripId);
 
-    return <div className="container mx-auto">
-             <TripHeader trip={trip}/>
-            </div>;
-    
+  if (!trip) return null;
+
+  return (
+    <div className="container mx-auto lg:px-40 lg:pt-10">
+      <TripHeader trip={trip} />
+      <div className="flex flex-col lg:flex-row lg:mt-12 lg:gap-20">
+        <div className="lg:order-2">
+          <TripReservation
+            trip={trip}
+            tripId={trip.id}
+            pricePerDay={trip.pricePerDay as any}
+            tripStartDate={trip.startDate}
+            tripEndDate={trip.endDate}
+            maxGuests={trip.maxGuests}
+          />
+       </div>
+       
+      </div>
+      <TripDescription description={trip.description}/>
+     
+    </div>
+  );
 };
+
 export default TripDetails;
