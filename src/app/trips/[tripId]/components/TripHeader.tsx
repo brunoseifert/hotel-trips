aac92730921e89e5f4b8b9bc9aa0fbb69e406c70
Bruno Seifert <128076381+brunoseifert@users.noreply.github.com>
2023-07-18 15:23:12 -0300

    location components

---
+++ src/app/trips/[tripId]/components/TripHeader.tsx
@@ -0,0 +1,103 @@
+import React from "react";
+import ReactCountryFlag from "react-country-flag";
+import Image from "next/image";
+import { Trip } from "@prisma/client";
+
+interface TripHeaderProps {
+  trip: Trip;
+}
+
+const TripHeader = ({ trip }: TripHeaderProps) => {
+  return (
+    <div className="flex flex-col">
+      <div className="relative h-[300px] w-full lg:hidden">
+        <Image
+          src={trip.coverImage}
+          fill
+          style={{
+            objectFit: "cover",
+          }}
+          alt={trip.name}
+        />
+      </div>
+
+      <div className="hidden lg:grid grid-cols-[2fr,1fr,1fr] gap-2 grid-rows-2 lg:order-2">
+        <div className="relative row-span-2">
+          <Image
+            src={trip.coverImage}
+            fill
+            style={{
+              objectFit: "cover",
+            }}
+            alt={trip.name}
+            className="rounded-tl-lg rounded-bl-lg shadow-md"
+          />
+        </div>
+
+        <div className="relative h-[200px] w-full">
+          <Image
+            src={trip.imagesUrl[0]}
+            fill
+            style={{
+              objectFit: "cover",
+            }}
+            alt={trip.name}
+            className="shadow-md"
+          />
+        </div>
+
+        <div className="relative h-[200px] w-full">
+          <Image
+            src={trip.imagesUrl[1]}
+            fill
+            style={{
+              objectFit: "cover",
+            }}
+            alt={trip.name}
+            className="shadow-md  rounded-tr-lg"
+          />
+        </div>
+
+        <div className="relative h-[200px] w-full">
+          <Image
+            src={trip.imagesUrl[2]}
+            fill
+            style={{
+              objectFit: "cover",
+            }}
+            alt={trip.name}
+            className="shadow-md"
+          />
+        </div>
+
+        <div className="relative h-[200px] w-full">
+          <Image
+            src={trip.coverImage}
+            fill
+            style={{
+              objectFit: "cover",
+            }}
+            alt={trip.name}
+            className="shadow-md  rounded-br-lg"
+          />
+        </div>
+      </div>
+
+      {/* TÍTULO E INFORMAÇÕES */}
+      <div className="flex flex-col p-5 lg:order-1 lg:p-0 lg:mb-10">
+        <h1 className="font-semibold text-xl lg:text-3xl text-primaryDarker">{trip.name}</h1>
+
+        <div className="flex items-center gap-1 my-1">
+          <ReactCountryFlag countryCode={trip.countryCode} svg />
+          <p className="text-xs lg:text-base text-grayPrimary underline">{trip.location}</p>
+        </div>
+
+        <p className="text-xs text-grayPrimary lg:hidden">
+          <span className="text-primary font-medium">R${trip.pricePerDay.toString()}</span> por dia
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TripHeader;
--- src/app/trips/[tripId]/page.tsx
+++ src/app/trips/[tripId]/page.tsx
@@ -5,6 +5,7 @@ import TripHeader from "./components/TripHeader";
 import TripReservation from "./components/TripReservation";
 import TripDescription from "./components/TripDescription";
 import TripHighlights from "./components/TripHighlights";
+import TripLocation from "./components/TripLocation";
 
 const getTripDetails = async (tripId: string) => {
   const trip = await prisma.trip.findUnique({
@@ -39,6 +40,8 @@ const TripDetails = async ({ params }: { params: { tripId: string } }) => {
       </div>
       <TripDescription description={trip.description}/>
       <TripHighlights highlights={trip.highlights}/>
+      <TripLocation  location={trip.location} locationDescription={trip.locationDescription}/>
+      
      
     </div>
   );
