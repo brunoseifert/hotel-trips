4b6de6abf04b3890baf05fcedfec865af945c44f
Bruno Seifert <128076381+brunoseifert@users.noreply.github.com>
2023-07-18 16:15:55 -0300

    inputs validad

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -9,30 +9,164 @@ import React from "react";
 import { Controller, useForm } from "react-hook-form";
 
 interface TripReservationProps {
-  trip: Trip
+  tripId: string;
+  tripStartDate: Date;
+  tripEndDate: Date;
+  maxGuests: number;
+  pricePerDay: number;
 }
 
-const TripReservation = ({trip}: TripReservationProps) => {
-  return(
-    <div>
-        <div className="flex flex-col px-5">
-          <div className="flex gap-4">
-            <DatePicker placeholderText="Data de início" onChange={() =>{}} className="w-full"/>
-            <DatePicker placeholderText="Data final" onChange={() =>{}} className="w-full"/>
-          </div>
+interface TripReservationForm {
+  guests: number;
+  startDate: Date | null;
+  endDate: Date | null;
+}
 
-          <Input placeholder={`Número de hóspedes (max:${trip.maxGuests})`}className="mt-4"/>
-          <div className="flex justify-between mt-2">
-            <p className="font-mediu text-sm text-primaryDarker">Total: </p>
-            <p className="font-mediu text-sm text-primaryDarker">R$ 2.500 </p>
-          </div>
+const TripReservation = ({ tripId, maxGuests, tripStartDate, tripEndDate, pricePerDay }: TripReservationProps) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control,
+    watch,
+    setError,
+  } = useForm<TripReservationForm>();
 
-          <div className="pb-10 border-b border-b-primaryLighter w-full">
-            <Button className="mt-3 w-full">Reservar agora</Button>
-          </div>
-        </div>
+  const router = useRouter();
+
+  const onSubmit = async (data: TripReservationForm) => {
+    const response = await fetch("/api/trips/check", {
+      method: "POST",
+      body: Buffer.from(
+        JSON.stringify({
+          startDate: data.startDate,
+          endDate: data.endDate,
+          tripId,
+        })
+      ),
+    });
+
+    const res = await response.json();
+
+    if (res?.error?.code === "TRIP_ALREADY_RESERVED") {
+      setError("startDate", {
+        type: "manual",
+        message: "Esta data já está reservada.",
+      });
+
+      return setError("endDate", {
+        type: "manual",
+        message: "Esta data já está reservada.",
+      });
+    }
+
+    if (res?.error?.code === "INVALID_START_DATE") {
+      return setError("startDate", {
+        type: "manual",
+        message: "Data inválida.",
+      });
+    }
+
+    if (res?.error?.code === "INVALID_END_DATE") {
+      return setError("endDate", {
+        type: "manual",
+        message: "Data inválida.",
+      });
+    }
+
+    router.push(
+      `/trips/${tripId}/confirmation?startDate=${data.startDate?.toISOString()}&endDate=${data.endDate?.toISOString()}&guests=${data.guests}`
+    );
+  };
+
+  const startDate = watch("startDate");
+  const endDate = watch("endDate");
+
+  return (
+    <div className="flex flex-col px-5 lg:min-w-[380px] lg:p-5 lg:border-grayLighter lg:border lg:rounded-lg lg:shadow-md">
+      <p className="text-xl hidden text-primaryDarker mb-4 lg:block">
+        <span className="font-semibold">R${pricePerDay}</span> por dia
+      </p>
+
+      <div className="flex gap-4">
+        <Controller
+          name="startDate"
+          rules={{
+            required: {
+              value: true,
+              message: "Data inicial é obrigatória.",
+            },
+          }}
+          control={control}
+          render={({ field }) => (
+            <DatePicker
+              error={!!errors?.startDate}
+              errorMessage={errors?.startDate?.message}
+              onChange={field.onChange}
+              selected={field.value}
+              placeholderText="Data de Início"
+              className="w-full"
+              minDate={tripStartDate}
+            />
+          )}
+        />
+
+        <Controller
+          name="endDate"
+          rules={{
+            required: {
+              value: true,
+              message: "Data final é obrigatória.",
+            },
+          }}
+          control={control}
+          render={({ field }) => (
+            <DatePicker
+              error={!!errors?.endDate}
+              errorMessage={errors?.endDate?.message}
+              onChange={field.onChange}
+              selected={field.value}
+              placeholderText="Data Final"
+              className="w-full"
+              maxDate={tripEndDate}
+              minDate={startDate ?? tripStartDate}
+            />
+          )}
+        />
+      </div>
+
+      <Input
+        {...register("guests", {
+          required: {
+            value: true,
+            message: "Número de hóspedes é obrigatório.",
+          },
+          max: {
+            value: maxGuests,
+            message: `Número de hóspedes não pode ser maior que ${maxGuests}.`,
+          },
+        })}
+        placeholder={`Número de hóspedes (max: ${maxGuests})`}
+        className="mt-4"
+        error={!!errors?.guests}
+        errorMessage={errors?.guests?.message}
+        type="number"
+      />
+
+      <div className="flex justify-between mt-3">
+        <p className="font-medium text-sm text-primaryDarker">Total: </p>
+        <p className="font-medium text-sm text-primaryDarker">
+          {startDate && endDate ? `R$${differenceInDays(endDate, startDate) * pricePerDay}` ?? 1 : "R$0"}
+        </p>
+      </div>
+
+      <div className="pb-10 border-b border-b-grayLighter w-full lg:border-none lg:pb-0">
+        <Button onClick={() => handleSubmit(onSubmit)()} className="mt-3 w-full">
+          Reservar agora
+        </Button>
+      </div>
     </div>
-  )
+  );
 };
 
 export default TripReservation;
