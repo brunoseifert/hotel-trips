c460d48af8c02fb3677e1fe962d09d9c1a0ac80c
Bruno Seifert <128076381+brunoseifert@users.noreply.github.com>
2023-07-21 16:27:39 -0300

    data

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,37 +1,43 @@
-import { LegacyRef, forwardRef } from "react";
-import { twMerge } from "tailwind-merge";
-import _DatePicker, { ReactDatePickerProps, registerLocale } from "react-datepicker";
-import ptBR from "date-fns/locale/pt-BR";
+import { LegacyRef, forwardRef } from 'react'
+import { twMerge } from 'tailwind-merge'
+import _DatePicker, {
+  ReactDatePickerProps,
+  registerLocale,
+} from 'react-datepicker'
+import ptBR from 'date-fns/locale/pt-BR'
+import 'react-datepicker/dist/react-datepicker.css'
 
-import "react-datepicker/dist/react-datepicker.css";
-
-registerLocale("pt-BR", ptBR);
+registerLocale('pt-BR', ptBR)
 
 interface InputProps extends ReactDatePickerProps {
-  error?: boolean;
-  errorMessage?: string;
+  error?: boolean
+  errorMessage?: string
 }
 
-function DatePicker({ className, error, errorMessage, ...props }: InputProps, ref: LegacyRef<HTMLInputElement> | undefined) {
-  const datePickerClassName = twMerge(
-    "rounded-lg border border-gray-300 bg-white p-2 text-sm font-normal text-primaryDarker placeholder-black placeholder-opacity-20 outline-none transition-all focus:ring-1 focus:ring-primary",
-    error ? "border-red-500" : "",
-    className
-  );
+function DatePicker(
+  { className, error, errorMessage, ...props }: InputProps,
+  ref: LegacyRef<HTMLInputElement> | undefined,
+) {
+  const inputClassName = twMerge(
+    className,
+    'rounded-lg border border-gray-300 bg-white p-2 text-sm font-normal text-dark placeholder-black placeholder-opacity-20 outline-none transition-all focus:ring-1 focus:ring-primary',
+    error ? 'border-red-500' : '',
+  )
 
   return (
     <div className="flex w-full flex-col">
       <_DatePicker
-        dateFormat="dd/MM/yyyy"
         locale="pt-BR"
         wrapperClassName="w-full"
-        className={datePickerClassName}
+        className={inputClassName}
         enableTabLoop={false}
         {...props}
       />
-      {error && errorMessage && <div className="text-red-500 mt-1 text-xs">{errorMessage}</div>}
+      {error && errorMessage && (
+        <div className="mt-1 text-xs text-red-500">{errorMessage}</div>
+      )}
     </div>
-  );
+  )
 }
 
-export default forwardRef(DatePicker);
+export default forwardRef(DatePicker)
