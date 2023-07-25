2f76c33ef511933159a82ac08a2be6829410cb8d
Bruno Seifert <128076381+brunoseifert@users.noreply.github.com>
2023-07-25 11:48:59 -0300

    deploy

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -1,172 +1,47 @@
-"use client";
-
 import Button from "@/components/Button";
-import DatePicker from "@/components/DatePicker";
-import Input from "@/components/Input";
-import { differenceInDays } from "date-fns";
-import { useRouter } from "next/navigation";
+import Image from "next/image";
 import React from "react";
-import { Controller, useForm } from "react-hook-form";
 
-interface TripReservationProps {
-  tripId: string;
-  tripStartDate: Date;
-  tripEndDate: Date;
-  maxGuests: number;
-  pricePerDay: number;
+interface TripLocationProps {
+  location: string;
+  locationDescription: string;
 }
 
-interface TripReservationForm {
-  guests: number;
-  startDate: Date | null;
-  endDate: Date | null;
-}
-
-const TripReservation = ({ tripId, maxGuests, tripStartDate, tripEndDate, pricePerDay }: TripReservationProps) => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    control,
-    watch,
-    setError,
-  } = useForm<TripReservationForm>();
-
-  const router = useRouter();
-
-  const onSubmit = async (data: TripReservationForm) => {
-    const response = await fetch("/api/trips/check", {
-      method: "POST",
-      body: Buffer.from(
-        JSON.stringify({
-          startDate: data.startDate,
-          endDate: data.endDate,
-          tripId,
-        })
-      ),
-    });
-
-    const res = await response.json();
-
-    if (res?.error?.code === "TRIP_ALREADY_RESERVED") {
-      setError("startDate", {
-        type: "manual",
-        message: "Esta data já está reservada.",
-      });
-
-      return setError("endDate", {
-        type: "manual",
-        message: "Esta data já está reservada.",
-      });
-    }
-
-    if (res?.error?.code === "INVALID_START_DATE") {
-      return setError("startDate", {
-        type: "manual",
-        message: "Data inválida.",
-      });
-    }
-
-    if (res?.error?.code === "INVALID_END_DATE") {
-      return setError("endDate", {
-        type: "manual",
-        message: "Data inválida.",
-      });
-    }
-
-    router.push(
-      `/trips/${tripId}/confirmation?startDate=${data.startDate?.toISOString()}&endDate=${data.endDate?.toISOString()}&guests=${data.guests}`
-    );
-  };
-
-  const startDate = watch("startDate");
-  const endDate = watch("endDate");
-
+const TripLocation = ({ location, locationDescription }: TripLocationProps) => {
   return (
-    <div className="flex flex-col px-5 lg:min-w-[380px] lg:p-5 lg:border-grayLighter lg:border lg:rounded-lg lg:shadow-md">
-      <p className="text-xl hidden text-primaryDarker mb-4 lg:block">
-        <span className="font-semibold">R${pricePerDay}</span> por dia
-      </p>
-
-      <div className="flex gap-4">
-        <Controller
-          name="startDate"
-          rules={{
-            required: {
-              value: true,
-              message: "Data inicial é obrigatória.",
-            },
+    <div className="p-5 lg:p-0 lg:mt-12 lg:pb-20">
+      <h2 className="font-semibold text-primaryDarker mb-5 lg:text-xl">Localização</h2>
+      <div className="relative h-[280px] w-full lg:hidden">
+        <Image
+          src="/map-mobile.png"
+          alt={location}
+          fill
+          style={{
+            objectFit: "cover",
           }}
-          control={control}
-          render={({ field }) => (
-            <DatePicker
-              error={!!errors?.startDate}
-              errorMessage={errors?.startDate?.message}
-              onChange={field.onChange}
-              selected={field.value}
-              placeholderText="Data de Início"
-              className="w-full"
-              minDate={tripStartDate}
-            />
-          )}
-        />
-
-        <Controller
-          name="endDate"
-          rules={{
-            required: {
-              value: true,
-              message: "Data final é obrigatória.",
-            },
-          }}
-          control={control}
-          render={({ field }) => (
-            <DatePicker
-              error={!!errors?.endDate}
-              errorMessage={errors?.endDate?.message}
-              onChange={field.onChange}
-              selected={field.value}
-              placeholderText="Data Final"
-              className="w-full"
-              maxDate={tripEndDate}
-              minDate={startDate ?? tripStartDate}
-            />
-          )}
+          className="rounded-lg shadow-md"
         />
       </div>
 
-      <Input
-        {...register("guests", {
-          required: {
-            value: true,
-            message: "Número de hóspedes é obrigatório.",
-          },
-          max: {
-            value: maxGuests,
-            message: `Número de hóspedes não pode ser maior que ${maxGuests}.`,
-          },
-        })}
-        placeholder={`Número de hóspedes (max: ${maxGuests})`}
-        className="mt-4"
-        error={!!errors?.guests}
-        errorMessage={errors?.guests?.message}
-        type="number"
-      />
-
-      <div className="flex justify-between mt-3">
-        <p className="font-medium text-sm text-primaryDarker">Total: </p>
-        <p className="font-medium text-sm text-primaryDarker">
-          {startDate && endDate ? `R$${differenceInDays(endDate, startDate) * pricePerDay}` ?? 1 : "R$0"}
-        </p>
+      <div className="relative h-[480px] w-full hidden lg:block">
+        <Image
+          src="/map-desktop.png"
+          alt={location}
+          fill
+          style={{
+            objectFit: "cover",
+          }}
+          className="rounded-lg shadow-md"
+        />
       </div>
 
-      <div className="pb-10 border-b border-b-grayLighter w-full lg:border-none lg:pb-0">
-        <Button onClick={() => handleSubmit(onSubmit)()} className="mt-3 w-full">
-          Reservar agora
-        </Button>
-      </div>
+      <h3 className="text-primaryDarker text-sm font-semibold mt-3 lg:text-base lg:mt-5">{location}</h3>
+      <p className="text-xs text-primaryDarker mt-2 leading-5 lg:text-sm lg:mt-4">{locationDescription}</p>
+      <Button variant="outlined" className="w-full mt-5">
+        Ver no Google Maps
+      </Button>
     </div>
   );
 };
 
-export default TripReservation;
+export default TripLocation;
