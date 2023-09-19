23b654c03d2a84841a1d6f8a5aa58145e1994747
Bruno Seifert <128076381+brunoseifert@users.noreply.github.com>
2023-09-18 23:41:16 -0300

    Update TripSearch.tsx

--- src/app/components/TripSearch.tsx
+++ src/app/components/TripSearch.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import Button from "@/components/Button";
 import CurrencyInput from "@/components/CurrencyInput";
 import DatePicker from "@/components/DatePicker";
@@ -6,27 +7,33 @@ import Input from "@/components/Input";
 import { useRouter } from "next/navigation";
 import React from "react";
 import { Controller, useForm } from "react-hook-form";
+
 interface TripSearchForm {
   text: string;
   startDate: Date | null;
   budget: string;
 }
+
 const TripSearch = () => {
   const router = useRouter();
+
   const {
     control,
     formState: { errors },
     register,
     handleSubmit,
   } = useForm<TripSearchForm>();
+
   const onSubmit = (data: TripSearchForm) => {
     router.push(`/trips/search?text=${data.text}&startDate=${data.startDate?.toISOString()}&budget=${data.budget}`);
   };
+
   return (
     <div className="container mx-auto p-5 bg-search-background bg-cover bg-center bg-no-repeat lg:py-28">
       <h1 className="font-semibold text-2xl text-primaryDarker text-center lg:text-[2.5rem]">
         Encontre sua próxima <span className="text-primary">viagem!</span>
       </h1>
+
       <div className="flex flex-col gap-4 mt-5 lg:flex-row lg:max-w-[948px] lg:mx-auto lg:p-4 lg:bg-primary lg:mt-12 lg:bg-opacity-20 lg:rounded-lg">
         <Input
           placeholder="Onde você quer ir?"
@@ -39,6 +46,7 @@ const TripSearch = () => {
             },
           })}
         />
+
         <div className="flex gap-4 lg:w-full">
           <Controller
             name="startDate"
@@ -47,6 +55,7 @@ const TripSearch = () => {
               <DatePicker onChange={field.onChange} selected={field.value} placeholderText="Data Final" className="w-full" minDate={new Date()} />
             )}
           />
+
           <Controller
             name="budget"
             control={control}
@@ -62,7 +71,6 @@ const TripSearch = () => {
           />
         </div>
 
-        <Button onClick={() => handleSubmit(onSubmit)()} className="w-1/2 lg:h-fit">
         <Button onClick={() => handleSubmit(onSubmit)()} className="w-full lg:h-fit">
           Buscar
         </Button>
@@ -70,4 +78,5 @@ const TripSearch = () => {
     </div>
   );
 };
+
 export default TripSearch;
