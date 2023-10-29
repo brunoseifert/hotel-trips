7ff50b088ed22cfb286af50c2eb8c9d03daffbb1
Bruno Seifert <128076381+brunoseifert@users.noreply.github.com>
2023-10-29 17:09:39 -0300

    chore: responsive mobile

--- src/app/components/RecommendedTrips.tsx
+++ src/app/components/RecommendedTrips.tsx
@@ -1,7 +1,7 @@
-import TripItem from "@/components/TripItem";
-import { prisma } from "@/lib/prisma";
-import { Trip } from "@prisma/client";
-import React from "react";
+import TripItem from '@/components/TripItem';
+import { prisma } from '@/lib/prisma';
+import { Trip } from '@prisma/client';
+import React from 'react';
 
 async function getTrips() {
   const trips = await prisma.trip.findMany({});
@@ -16,11 +16,13 @@ const RecommendedTrips = async () => {
     <div className="container mx-auto p-5">
       <div className="flex items-center">
         <div className="w-full h-[1px] bg-grayLighter"></div>
-        <h2 className="px-5 font-medium text-grayPrimary whitespace-nowrap">Destinos Recomendados</h2>
+        <h2 className="px-5 font-medium text-grayPrimary whitespace-nowrap">
+          Destinos Recomendados
+        </h2>
         <div className="w-full h-[1px] bg-grayLighter"></div>
       </div>
 
-      <div className="flex flex-col items-center mt-5 lg:mt-12 gap-5 lg:flex-row gap lg:flex-wrap lg:justify-center lg:gap-10">
+      <div className="flex flex-col items-center mt-5 md:mt-12 gap-5 md:flex-row gap md:flex-wrap  md:justify-center md:gap-10">
         {data.map((trip: Trip) => (
           <TripItem key={trip.id} trip={trip} />
         ))}
