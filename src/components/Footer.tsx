516fef87c26bb4b07c498f44af32e8d971349563
Bruno Seifert <128076381+brunoseifert@users.noreply.github.com>
2023-11-25 18:05:08 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="bg-walterWhite p-5 justify-center flex flex-col items-center">
-      <Image src="/logo.png" width={133} height={23} alt="Full Stack Week" />
+      <Image src="/Logo.png" width={133} height={23} alt="Full Stack Week" />
       <p className="text-sm font-medium mt-1 text-primaryDarker">Todos os direitos reservados.</p>
     </div>
   );
