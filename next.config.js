af84dff6943e2d517d48af231be692c705594dc3
Bruno Seifert <128076381+brunoseifert@users.noreply.github.com>
2024-08-09 12:51:34 -0300

    Revert "core: changed seed images"
    This reverts commit c5b33bda6a1f18925702a6f41a084a7cf29a38c9.

--- next.config.js
+++ next.config.js
@@ -2,24 +2,23 @@
 const nextConfig = {
   images: {
     domains: [
-      "cf.bstatic.com",
-      "imgcy.trivago.com",
-      "lh3.googleusercontent.com",
-      "images.unsplash.com",
-      "a0.muscache.com",
-      "utfs.io",
+      'cf.bstatic.com',
+      'imgcy.trivago.com',
+      'lh3.googleusercontent.com',
+      'images.unsplash.com',
+      'a0.muscache.com',
     ],
 
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-        port: "",
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
       },
       {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        port: "",
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
       },
     ],
   },
